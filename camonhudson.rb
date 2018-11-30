7e4abfbe62ad32b0fc9ed1cae87bcf2055d4658a
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-11-30 14:34:21 -0500

    Initialize testing.

---
+++ camonhudson.rb
@@ -0,0 +1,3 @@
+class CamOnHudson
+
+end
---
+++ spec/camonhudson_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+describe CamOnHudson do
+
+  context "when it is initialized, it" do
+    it "looks to see if there is a schedule for it."
+  end
+
+end
