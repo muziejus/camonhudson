e0afd2773ed5a38ac51cd6146bfe9d33cd6b7b70
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-11-30 23:04:39 -0500

    Add test to check for schedule.

--- camonhudson.rb
+++ camonhudson.rb
@@ -5,8 +5,19 @@ class CamOnHudson
 
   def initialize
     @today = Date.today.strftime "%Y-%m-%d"
-    @schedule = "ping" #schedule
+    @schedule = schedule
   end
 
+  private
+
+  def schedule
+    schedule_file = "#{@today}-schedule.yml"
+    if File.exists? schedule_file
+      YAML::load_file schedule_file
+    # else
+    #   create_schedule
+    end
+    
+  end
 
 end
--- spec/camonhudson_spec.rb
+++ spec/camonhudson_spec.rb
@@ -8,14 +8,32 @@ describe CamOnHudson do
   context "#initialize" do
     it "sets @today" do
       today = Date.today.strftime "%Y-%m-%d"
-      expect(cam.instance_variable_get(:@today)).to eq today
+      expect(cam.instance_variable_get :@today ).to eq today
     end
     context "loads the day's schedule" do
+
+      let(:local_cam){
+        today = Date.today.strftime "%Y-%m-%d"
+        File.open("#{today}-schedule.yml", "w") do |file|
+          file.puts YAML::dump({ sunrise: "foo", sunset: "bar" })
+        end
+        local_cam = CamOnHudson.new
+        File.delete("#{today}-schedule.yml")
+        local_cam
+      }
+      
       it "as @schedule" do
-        expect(cam.instance_variable_get(:@schedule)).not_to be_nil
+        expect(local_cam.instance_variable_get :@schedule ).not_to be_nil
+      end
+
+      it "from a yaml file called yyyy-mm-dd-schedule.yml" do
+        expect(local_cam.instance_variable_get :@schedule ).to be_a Hash
       end
     end
-    it "loads the day's schedule, in a file called yyyy-mm-dd-schedule.yml"
+
+    context "else it creates a new schedule"
+
+    it "loads the day's schedule, in a file "
     it "builds a schedule if none exists"
   end
 
