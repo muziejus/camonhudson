142d6c162e2577e6fbc6d7cc0cdb12cbff72ccb7
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-12-02 08:05:05 -0500

    Refactor sun_schedule tests.

--- spec/camonhudson_spec.rb
+++ spec/camonhudson_spec.rb
@@ -25,6 +25,8 @@ describe CamOnHudson do
 
   context "@configs" do
 
+    let(:configs){ CamOnHudson.new.instance_variable_get(:@configs) }
+
     context "When there is no 'configs.yml'" do
       it "'configs.yml' is created anew" do
         config_file = "configs.yml"
@@ -34,39 +36,28 @@ describe CamOnHudson do
       end
     end
 
+    it "has a :sun_schedule" do
+      expect(configs.keys.include? :sun_schedule).to be true
+    end
+
+    context ":sun_schedule" do
+      it "has the keys :sunrise and :sunset" do
+        expect(configs[:sun_schedule].keys).to contain_exactly :sunrise, :sunset
+      end
+
+      it "which are Times" do
+        types = configs[:sun_schedule].values.select{ |v| v.class != Time }
+        expect(types.length).to eq 0
+      end
+
+      it "which are in the local time zone" do
+        offset = Time.now.utc_offset
+        zones = configs[:sun_schedule].values.select{ |v| v.utc_offset != offset }
+        puts zones
+        expect(zones.length).to eq 0
+      end
+    end
+
   end
-
-      # it "as @schedule" do
-      #   expect(local_cam.instance_variable_get :@schedule ).not_to be_nil
-      # end
-
-      # it "from a yaml file called yyyy-mm-dd-schedule.yml" do
-      #   expect(local_cam.instance_variable_get :@schedule ).to be_a Hash
-      # end
-
-      # it "has the keys :sunrise and :sunset" do
-      #   expect(local_cam.instance_variable_get(:@schedule).keys).to contain_exactly :sunrise, :sunset
-      # end
-    # end
-
-    # context "else it creates a new schedule" do
-
-      # let(:schedule){ cam.instance_variable_get(:@schedule) }
-
-      # it "has the keys :sunrise and :sunset" do
-      #   expect(schedule.keys).to contain_exactly :sunrise, :sunset
-      # end
-
-      # it "has keys that are Times" do
-      #   types = schedule.map{ |k,v| v.class }.uniq
-      #   expect(types[0]).to eq Time
-      # end
-
-      # it "has keys that are *only* Times" do
-      #   types = schedule.map{ |k,v| v.class }.uniq
-      #   expect(types.length).to eq 1
-      # end
-    # end
-  # end
 
 end
