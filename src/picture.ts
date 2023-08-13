33664f01c5b5c1ce30348e0b797a8020d77f5ffd
Moacir <github@moacir.moacir.com>
2023-08-13 00:56:20 -0400

    feat: Setup Pi Camera

--- src/picture.ts
+++ src/picture.ts
@@ -3,14 +3,17 @@ import PiCamera from "pi-camera";
 import fs from "fs";
 
 export default async function picture(agent: BskyAgent){
+  const output = `${__dirname}/photo.png`;
+
   const camera = new PiCamera({
     mode: "photo",
-    output: `${__dirname}/test.png`,
+    output,
     width: 1920,
     height: 1080,
     nopreview: true
   });
-  // await camera.snap()
+  await camera.snap()
   // const image = fs.readFileSync("camonhudson-test.png");
-  // return agent.uploadBlob(image, {encoding: "image/png"});
+  const image = fs.readFileSync(output);
+  return agent.uploadBlob(image, {encoding: "image/png"});
 }
