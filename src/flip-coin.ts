4912b0ec4fa1b4a26bfa58e9a8053ba3fd8efa38
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2024-10-20 15:41:23 -0400

    feat: Add moon phases.

--- src/flip-coin.ts
+++ src/flip-coin.ts
@@ -1,19 +1,23 @@
 import sunCalc from "suncalc";
 
-export default function(rate: number, latitude: number, longitude: number){
+export default function (rate: number, latitude: number, longitude: number) {
   const now = new Date();
   const times = sunCalc.getTimes(new Date(), latitude, longitude);
 
-  if(+now - +times.sunrise < 120000 && +now - +times.sunrise > 0){
+  if (+now - +times.sunrise < 120000 && +now - +times.sunrise > 0) {
     return "sunrise";
   }
 
-  if(+now - +times.sunset > -120000 && +now - +times.sunset < 0){
+  if (+now - +times.sunset > -120000 && +now - +times.sunset < 0) {
     return "sunset";
   }
 
-  if(Math.random() < rate){
-    return "post";
+  if (Math.random() < rate) {
+    if (+now > +times.sunset || +now < +times.sunrise) {
+      return "night";
+    }
+
+    return "day";
   }
 
   return "";
--- src/get-skeet-text.ts
+++ src/get-skeet-text.ts
@@ -1,4 +1,6 @@
-export default function(time: "sunrise" | "sunset" | "post"){
+import sunCalc from "suncalc";
+
+export default function (time: "sunrise" | "sunset" | "night" | "day") {
   const now = new Date().toLocaleString("en-US", {
     timeZone: "America/New_York",
     weekday: "long",
@@ -9,14 +11,37 @@ export default function(time: "sunrise" | "sunset" | "post"){
     hour12: false,
     minute: "numeric",
   });
-  
-  if(time == "sunrise"){
+
+  if (time == "sunrise") {
     return `🌅 on ${now} in New York City.`;
   }
 
-  if(time == "sunset"){
+  if (time == "sunset") {
     return `🌇 on ${now} in New York City.`;
   }
-  
+
+  if (time == "night") {
+    const moonPhase = sunCalc.getMoonIllumination(new Date()).phase;
+    const moonPhases = ["🌑", "🌒", "🌓", "🌔", "🌕", "🌖", "🌗", "🌘"];
+    let moon = moonPhases[0];
+    if (moonPhase > 0 && moonPhase < 0.25) {
+      moon = moonPhases[1];
+    } else if (moonPhase == 0.25) {
+      moon = moonPhases[2];
+    } else if (moonPhase < 0.5) {
+      moon = moonPhases[3];
+    } else if (moonPhase == 0.5) {
+      moon = moonPhases[4];
+    } else if (moonPhase > 0.5 && moonPhase < 0.75) {
+      moon = moonPhases[5];
+    } else if (moonPhase == 0.75) {
+      moon = moonPhases[6];
+    } else if (moonPhase < 1) {
+      moon = moonPhases[7];
+    }
+
+    return `${moon} on ${now} in New York City.`;
+  }
+
   return `Greetings from 🗽 on ${now}.`;
 }
